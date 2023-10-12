d243adbe0001a34ee497cc1772246f9e28b372c7
Hernán Asorey <asoreyh@gmail.com>
2023-10-13 00:05:12 +0200

    version added

--- lago-anna.sh
+++ lago-anna.sh
@@ -10,15 +10,13 @@
 # License:     BSD-3-Clause
 # See the LICENSE file in the project root for full license information.
 ################################################################################
-VERSION="1.5"
+VERSION="1.5.0"
 export LAGO_ANNA=${PWD}
-export LAGO_ANNA_VERSION=${VERSION}
 date=$(date -u)
 echo "#
 ## Changes added by the LAGO ANNA suite on $date
 #
 export LAGO_ANNA=\"${LAGO_ANNA}\"
-export LAGO_ANNA_VERSION=\"${VERSION}\"
 export PATH=\"\${LAGO_ANNA}:\$PATH\"
 " >> ${HOME}/.bashrc
 source ${HOME}/.bashrc
